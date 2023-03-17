d7dfc2f46d42fa5d45582035df079d489bf9f942
denisabonda <denocheboy@gmail.com>
2023-03-17 05:03:41 -0700

    configured-cors

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ const app=express()
 
 connectDB()
 
-app.use(cors)
+app.use(cors({
+    origin:[
+        "http://localhost:3000","https://enterprice_dashboard.onrender.com"
+    ]
+}))
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
